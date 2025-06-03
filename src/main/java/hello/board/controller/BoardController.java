265a6c0408010e6363e0b2159340dcb8312bd41a
JungSoonIn <tnsdls1966@naver.com>
2025-06-03 16:42:29 +0900

    feat: add BoardDTO

--- src/main/java/hello/board/controller/BoardController.java
+++ src/main/java/hello/board/controller/BoardController.java
@@ -1,12 +1,20 @@
 package hello.board.controller;
 
+import hello.board.dto.BoardDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
     @GetMapping("/board/save")
     public String saveForm() {
         return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) {
+        return null;
     }
 }
